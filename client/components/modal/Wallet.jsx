58995fc7006ada3fcc48c0a11de0e9215d373f43
rhythmshandlya <armaanbgp@gmail.com>
2022-02-08 17:08:46 +0530

    valletn modal

--- client/components/modal/Wallet.jsx
+++ client/components/modal/Wallet.jsx
@@ -13,11 +13,14 @@ const Wallet = ({ status, setWalletModal }) => {
             <div className="modal-background"></div>
             <div className="card" style={modalStyle}>
                 <div className="card-content">
-                    <div className="content">
+                    <div className="content" className={style.dFlex}>
                         <a className="button is-light p-3" onClick={() => { setWalletModal("is-active") }}>
-                            <img src="https://docs.metamask.io/metamask-fox.svg" className='p-2'/>
+                            <img src="https://docs.metamask.io/metamask-fox.svg" className='p-2' />
                             Connect Metamask
                         </a>
+                        <img width="35px" className='pointer'
+                            onClick={() => { setWalletModal("") }}
+                            src="https://img.icons8.com/external-vitaliy-gorbachev-fill-vitaly-gorbachev/50/000000/external-close-sales-vitaliy-gorbachev-fill-vitaly-gorbachev.png" />
                     </div>
                 </div>
             </div>
